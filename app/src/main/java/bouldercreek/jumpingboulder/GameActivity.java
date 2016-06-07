bd7be323407fc9282ab6e9ae692e606b69d36120
s144122 <s144122@student.dtu.dk>
2016-06-07 12:38:21 +0200

    Game loop

--- app/src/main/java/bouldercreek/jumpingboulder/GameActivity.java
+++ app/src/main/java/bouldercreek/jumpingboulder/GameActivity.java
@@ -18,6 +18,6 @@ public class GameActivity extends AppCompatActivity{
         super.onCreate(savedInstanceState);
 
 
-        setContentView(R.layout.activity_game);
+        setContentView(new GamePanel(this));
     }
 }
