cd4bac912f88e6ee2473a82cbda488dc297f46cb
Jakob Rydhof <jakobrydhof@gmail.com>
2016-06-07 10:47:14 +0200

    Patched fra master

--- app/src/main/java/bouldercreek/jumpingboulder/MainActivity.java
+++ app/src/main/java/bouldercreek/jumpingboulder/MainActivity.java
@@ -1,6 +1,7 @@
 package bouldercreek.jumpingboulder;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,14 +9,16 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         //turn titel off
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         //set to full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -24,9 +27,19 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    public void quickGameButtonOnClick(View v){
-        Button button = (Button) v;
-        ((Button) v).setText("HEj");
 
+    button = (Button) findViewById(R.id.button);
+
+    button.setOnClickListener(new View.OnClickListener(){
+        @Override;
+        public void OnClick(View v){
+
+    }
+
+
+
+    public void quickGameButtonOnClick(View v){
+        Intent startGame = new Intent(this, GameActivity.class);
+        startActivity(startGame);
     }
 }
