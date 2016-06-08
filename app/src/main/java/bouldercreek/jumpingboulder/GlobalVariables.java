66222129fab645622732c504ec6dd6804c5b32dc
Jakob Rydhof <jakobrydhof@gmail.com>
2016-06-08 13:55:11 +0200

    Forbindelse kan nu oprettes til serveren

--- app/src/main/java/bouldercreek/jumpingboulder/GlobalVariables.java
+++ app/src/main/java/bouldercreek/jumpingboulder/GlobalVariables.java
@@ -1,7 +1,11 @@
 package bouldercreek.jumpingboulder;
 
+import java.net.Socket;
+
 /**
  * Created by jakob on 08-06-2016.
  */
 public class GlobalVariables {
+    public static Socket serverSocket;
+
 }
--- app/src/main/java/bouldercreek/jumpingboulder/MainActivity.java
+++ app/src/main/java/bouldercreek/jumpingboulder/MainActivity.java
@@ -9,6 +9,11 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,10 +30,42 @@ public class MainActivity extends AppCompatActivity {
 
 
         setContentView(R.layout.activity_main);
+
+        connectToServer();
+
     }
 
 
     public void quickGameButtonOnClick(View v){
-        startActivity(new Intent(this, QuickGameRoom.class));
+        //startActivity(new Intent(this, QuickGameRoom.class));
+        System.out.println(GlobalVariables.serverSocket);
+    }
+
+    private void connectToServer(){
+        final String serverIP = "10.16.172.118";
+        final int serverPort = 1302;
+
+        new Thread() {
+            public void run() {
+                try {
+                    System.out.println("Trying to connect to server " +
+                            "IP: "+serverIP + " " +
+                            "port: " + serverPort);
+                    GlobalVariables.serverSocket = new Socket(serverIP,serverPort);
+                    System.out.println("Server connection established");
+                    BufferedReader bf = new BufferedReader(
+                            new InputStreamReader(GlobalVariables.serverSocket.getInputStream()));
+                    System.out.println(bf.readLine());
+
+                } catch (IOException e) {
+                    System.out.println("Could not connect to server on"
+                            + " IP: " + serverIP
+                            + " Port: "+serverPort
+                    );
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
     }
 }
